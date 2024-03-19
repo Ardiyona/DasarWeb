1fddd3fa82276ea24597e4512ee621273ccd9c39
Ardiyona <rakhaardiyona98@gmail.com>
2024-03-19 13:05:13 +0700

    Pertemuan5 - Praktikum4

---
+++ Jobsheet 5/fungsi.php
@@ -0,0 +1,57 @@
+<?php
+// function perkenalan($nama, $salam){
+//     echo $salam . ", ";
+//     echo "Perkenalkan, nama saya " . $nama . "<br/>";
+//     echo "Senang berkenalan dengan anda<br/>";
+// }
+
+// perkenalan("Rakha","Hallo");
+
+// echo "<hr>";
+
+// $saya = "Elok";
+// $ucapanSalam = "Selamat pagi";
+
+// perkenalan($saya, $ucapanSalam);
+
+// perkenalan($saya, $ucapanSalam);
+
+// function perkenalan($nama, $salam = "Assalamualaikum"){
+//     echo $salam . ", ";
+//     echo "Perkenalkan, nama saya " . $nama . "<br/>";
+//     echo "Senang berkenalan dengan anda<br/>";
+// }
+
+// perkenalan("Rakha","Hallo");
+
+// echo "<hr>";
+
+// $saya = "Elok";
+// $ucapanSalam = "Selamat pagi";
+
+// perkenalan($saya);
+
+// function hitungUmur($thn_lahir, $thn_sekarang){
+//     $umur = $thn_sekarang - $thn_lahir;
+//     return $umur;
+// }
+
+// echo "Umur saya adalah " . hitungUmur(2004, 2024) . " tahun";
+
+// function hitungUmur($thn_lahir, $thn_sekarang){
+//     $umur = $thn_sekarang - $thn_lahir;
+//     return $umur;
+// }
+
+// function perkenalan($nama, $salam="Assalamualaikum"){
+//     echo $salam . ",";
+//     echo "Perkenalkan, nama saya " . $nama . "<br/>";
+
+//     echo "Saya berusia " . hitungUmur(2004, 2024) . " tahun<br/>";
+//     echo "Senang berkenalan dengan anda<br/>";
+// }
+
+// perkenalan("Rakha");
+
+
+?>
