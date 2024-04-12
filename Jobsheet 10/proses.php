738da666175a9b1920b8e3272418f5cb1aeb0a8c
Ardiyona <rakhaardiyona98@gmail.com>
2024-04-12 07:45:59 +0700

    Pertemuan10 - Praktikum04

--- Jobsheet 10/proses.php
+++ Jobsheet 10/proses.php
@@ -17,6 +17,44 @@
             echo "Gagal menambahkan data: " . mysqli_error($connect);
         }
     }
+    elseif ($aksi == 'ubah') {
+        if (isset($_POST['id'])) {
+            $id = $_POST['id'];
+
+            $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id=$id";
+            if (mysqli_query($connect, $query)) {
+                header("Location: index.php");
+                exit();
+            }
+            else {
+                echo "Gagal mengupdate data: " . mysqli_error($connect);
+            }
+        }
+        else {
+            echo "ID tidak valid";
+        }
+    }
+    elseif ($aksi == 'hapus') {
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+
+            $query = "DELETE FROM anggota WHERE id=$id";
+            
+            if (mysqli_query($connect, $query)) {
+                header("Location: index.php");
+                exit();
+            }
+            else {
+                echo "Gagal menghapus data: " . mysqli_error($connect);
+            }
+        }
+        else {
+            echo "ID tidak valid";
+        }
+    }
+    else {
+        header("Location: index.php");
+    }
 
     mysqli_close($connect);
 ?>
